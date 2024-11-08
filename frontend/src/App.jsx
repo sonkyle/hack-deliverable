e11b3939cf75c26091611ec59624b5975cdb3b02
Kyle Son <swson1@uci.edu>
2024-11-08 04:57:26 -0800

    Added a dropdown menu for sorting the age of quotes with basic CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,41 @@
 import "./App.css";
-import quotebook from './quotebook.png';
+import quotebook from "./quotebook.png";
 
 function App() {
-	return (
-		<div className="App">
-			{/* TODO: include an icon for the quote book */}
-			<img className="logo" src={quotebook} alt="quotebook logo goes here" />
-			<h1>Hack at UCI Tech Deliverable</h1>
-			
-			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
-				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
-				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
-				<button type="submit">Submit</button>
-			</form>
+  return (
+    <div className="App">
+      {/* TODO: include an icon for the quote book */}
+      <img className="logo" src={quotebook} alt="quotebook logo goes here" />
+      <h1>Hack at UCI Tech Deliverable</h1>
 
-			<h2>Previous Quotes</h2>
-			{/* TODO: Display the actual quotes from the database */}
-			<div className="messages">
-				<p>Peter Anteater</p>
-				<p>Zot Zot Zot!</p>
-				<p>Every day</p>
-			</div>
-		</div>
-	);
+      <h2>Submit a quote</h2>
+      {/* TODO: implement custom form submission logic to not refresh the page */}
+      <form action="/api/quote" method="post">
+        <label htmlFor="input-name">Name</label>
+        <input type="text" name="name" id="input-name" required />
+        <label htmlFor="input-message">Quote</label>
+        <input type="text" name="message" id="input-message" required />
+        <button type="submit">Submit</button>
+      </form>
+
+
+      <h2>Previous Quotes</h2>
+      {/* TODO: Display the actual quotes from the database */}
+
+      <div class="dropdown">
+        <button class="dropbtn">Sort By</button>
+        <div class="dropdown-content">
+          <a href="http://localhost:5173/quote?age=7">Last week</a>
+          <a href="http://localhost:5173/quote?age=30">Last month</a>
+          <a href="http://localhost:5173/quote?age=365">Last year</a>
+          <a href="http://localhost:5173/quote?age=">All time</a>
+        </div>
+      </div>
+
+      <div className="messages"></div>
+    </div>
+	
+  );
 }
 
 export default App;
